9b5062460bbd35cf7da5905ac0d6dd382e81c957
steph-ayo <popoolastephen19@gmail.com>
2023-12-29 19:27:56 +0100

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,7 @@
+export default function Navigation() {
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p className="f3 link dim black underline pa3 pointer">Sign out</p>
+    </nav>
+  );
+}
