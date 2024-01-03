8758518cf98b894a01157fb04251ae2a3310f108
steph-ayo <popoolastephen19@gmail.com>
2024-01-03 22:41:21 +0100

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import "./App.css";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 export default function App() {
   return (
     <div className="App">
+      <Particles className="Particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
